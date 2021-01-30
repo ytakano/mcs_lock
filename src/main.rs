add2b4fa425e739b6bb8fbe02f13cf174396a0c5
Yuuki Takano <ytakanoster@gmail.com>
2021-01-30 20:35:07 +0900

    refactored

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ struct MCSLock<T> {
 struct MCSNode<T> {
     next: AtomicPtr<MCSNode<T>>,
     locked: AtomicBool,
-    mcs_lock: Arc<MCSLock<T>>,
 }
 
 impl<T> MCSLock<T> {
@@ -23,52 +22,51 @@ impl<T> MCSLock<T> {
         }
     }
 
-    fn get_locker(self: Arc<MCSLock<T>>) -> MCSNode<T> {
-        MCSNode {
-            next: AtomicPtr::new(null_mut()),
-            locked: AtomicBool::new(false),
-            mcs_lock: self.clone(),
-        }
-    }
-}
-
-unsafe impl<T> Sync for MCSLock<T> {}
-unsafe impl<T> Send for MCSLock<T> {}
-
-impl<T> MCSNode<T> {
-    fn lock(&mut self) -> MCSLockGuard<T> {
+    fn lock(&self) -> MCSLockGuard<T> {
         // 自身をキューの最後尾とする
-        let ptr = self as *mut Self;
-        let prev = self.mcs_lock.last.swap(ptr, Ordering::SeqCst);
+        let mut guard = MCSLockGuard {
+            node: MCSNode {
+                next: AtomicPtr::new(null_mut()),
+                locked: AtomicBool::new(false),
+            },
+            mcs_lock: self,
+        };
+
+        let ptr = &mut guard.node as *mut MCSNode<T>;
+        let prev = self.last.swap(ptr, Ordering::SeqCst);
 
         // 最後尾がnullの場合は誰もロックを獲得しようとしていないためロック獲得
         // null以外の場合は、自身をキューの最後尾に追加
         if prev != null_mut() {
             // ロック獲得中と設定
-            self.locked.store(true, Ordering::SeqCst);
+            guard.node.locked.store(true, Ordering::SeqCst);
 
             // 自身をキューの最後尾に追加
             let prev = unsafe { &*prev };
             prev.next.store(ptr, Ordering::SeqCst);
 
             // 他のスレッドからfalseに設定されるまでスピン
-            while self.locked.load(Ordering::SeqCst) {}
+            while guard.node.locked.load(Ordering::SeqCst) {}
         }
 
-        MCSLockGuard { node: self }
+        guard
     }
 }
 
+unsafe impl<T> Sync for MCSLock<T> {}
+unsafe impl<T> Send for MCSLock<T> {}
+
 struct MCSLockGuard<'a, T> {
-    node: &'a mut MCSNode<T>,
+    node: MCSNode<T>,
+    mcs_lock: &'a MCSLock<T>,
 }
 
 impl<'a, T> Drop for MCSLockGuard<'a, T> {
     fn drop(&mut self) {
         // 自身の次のノードがnullかつ自身が最後尾のノードなら、最後尾をnullに設定
         if self.node.next.load(Ordering::SeqCst) == null_mut() {
-            let ptr = self.node as *mut MCSNode<T>;
-            if let Ok(_) = self.node.mcs_lock.last.compare_exchange(
+            let ptr = &mut self.node as *mut MCSNode<T>;
+            if let Ok(_) = self.mcs_lock.last.compare_exchange(
                 ptr,
                 null_mut(),
                 Ordering::SeqCst,
@@ -90,19 +88,19 @@ impl<'a, T> Drop for MCSLockGuard<'a, T> {
     }
 }
 
-// 保護対象データのimmutableな参照はずし (10)
+// 保護対象データのimmutableな参照はずし
 impl<'a, T> Deref for MCSLockGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe { &*self.node.mcs_lock.data.get() }
+        unsafe { &*self.mcs_lock.data.get() }
     }
 }
 
-// 保護対象データのmutableな参照はずし (11)
+// 保護対象データのmutableな参照はずし
 impl<'a, T> DerefMut for MCSLockGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { &mut *self.node.mcs_lock.data.get() }
+        unsafe { &mut *self.mcs_lock.data.get() }
     }
 }
 
@@ -117,10 +115,9 @@ fn main() {
         let lock0 = lock.clone();
         // スレッド生成
         let t = std::thread::spawn(move || {
-            let mut locker = lock0.get_locker();
             for _ in 0..NUM_LOOP {
                 // ロック
-                let mut data = locker.lock();
+                let mut data = lock0.lock();
                 *data += 1;
             }
         });
@@ -131,10 +128,9 @@ fn main() {
         t.join().unwrap();
     }
 
-    let mut locker = lock.get_locker();
     println!(
         "COUNT = {} (expected = {})",
-        *locker.lock(),
+        *lock.lock(),
         NUM_LOOP * NUM_THREADS
     );
 }
